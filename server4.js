08c88821404d9ce5f16144b7df3441f11dff9816
UAdeadUA-PC\UAdeadUA <korpach.nazar@gmail.com>
2020-07-29 20:00:39 +0300

    changes'

--- server4.js
+++ server4.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const { response } = require('express');
+const { compileFunction } = require('vm');
 const app = express()
 
 app.use(bodyParser({extended:  true}));
-var sql
 const sqlite3 = require('sqlite3').verbose();
 let db = new sqlite3.Database('./users.db', (err) => {
     if (err) {
@@ -12,80 +13,52 @@ let db = new sqlite3.Database('./users.db', (err) => {
     console.log("Connected to the in-memory SQlite database.");
 });
 
-app.post('/', function(request, response){
+app.post('/', async (request, response) =>{
     if (!request.body){
         return response.send('err 400')
     }
-    var mode 
-    for (i in request.body){
-        mode = i
-        break
-    }
-
-    const name = request.body[mode]
-    sql = `INSERT INTO users (name) VALUES ('${name}') `;
+    console.log(request.body)
+    const name = request.body.name
+    //console.log(name)
+    const password = request.body.password
+    const email = request.body.email
+    
+    sql = `INSERT INTO users (name, password, email) VALUES ('${name}', '${password}', '${email}'); `;
     console.log(sql)
-    let a = []
-    const prom = new Promise(function(resolve, reject){
+    response.send(await query(sql))
+});
+
+app.delete('/', async (request, response) =>{
+    const name = request.query.name
+    sql = `DELETE FROM users WHERE name =  ('${name}') `;
+    response.send(await query(sql))
+});
+
+const query  = (sql) => {
+    return new Promise((resolve, reject) => {
         const arr = []
         db.all(sql, [], (err, rows) => {
            if (err) {
-                throw err;
-            }
-            rows.forEach((row) => {
-                arr.push(row.name)
-                console.log(row.name);
-            });
-        resolve()
-        })
-    }).then(response.send(""));
-});
+               throw err;
+           }
+           rows.forEach((row) => {
+               arr.push({
+                name: row.name,
+                password: row.password,
+                email: row.email
+               })
+               console.log(row);
+           });
+           resolve(arr)
+       });
+   })
+}
 
-app.delete('/', function(request, response){
-    if (!request.body){
-        return response.send('err 400')
-    }
-    var mode 
-    for (i in request.body){
-        mode = i
-        break
-    }
-    const name = request.body[mode]
-    sql = `DELETE FROM users WHERE name =  ('${name}') `;
-    console.log(sql);
-    const prom = new Promise(function(resolve, reject){
-        const arr = []
-        db.all(sql, [], (err, rows) => {
-            if (err) {
-                throw err;
-            }
-            rows.forEach((row) => {
-                arr.push(row.name)
-                console.log(row.name);
-            });
-        resolve()
-        })
-    }).then(response.send(""));
-});
-
-app.get('/', function(request, response){
+app.get('/', async (request, response) => {
     sql = "SELECT * FROM users";
     console.log(sql)
-    const prom = new Promise(function(resolve, reject){
-        const arr = []
-        db.all(sql, [], (err, rows) => {
-           if (err) {
-                throw err;
-            }
-            rows.forEach((row) => {
-                arr.push(row.name)
-                console.log(row.name);
-            });
-        resolve(arr)
-        });
-    }).then(ar =>{
-        response.send(ar)
-    })
+    console.log(request.query)
+    response.send(await query(sql))
 })
 
 app.listen(3000)
