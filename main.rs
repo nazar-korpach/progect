77bce268eefc1744f7aa77d9af0dc2acbe3c9c8a
UAdeadUA-PC\UAdeadUA <korpach.nazar@gmail.com>
2020-08-14 16:41:55 +0300

    small changes

--- main.rs
+++ main.rs
@@ -5,31 +5,28 @@
 
 use rocket_contrib::databases::rusqlite;
 use rocket::response::content;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use rocket_contrib::json::Json;
 use sha2::{Sha256, Digest};
 use std::fmt::Write;
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize)]
 struct User{
     name: String,
     password: String,
     email: String
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize)]
 struct Member {
     name: String,
     password: String
 }
 
-
 #[database("sqlite_logs")]
 struct LogsDbConn(rusqlite::Connection);
 
 fn main() {
-    //let a = LogsDbConn("aa");
-    //strange(a);
     rocket::ignite()
        .attach(LogsDbConn::fairing())
        .mount("/", routes![get, new_user, delete, login])
@@ -48,20 +45,25 @@ fn del(connection: &LogsDbConn, name: String){
 
 #[post("/login", format = "application/json", data = "<user>")]
 fn login(user: Json<Member>, connection: LogsDbConn) -> content::Json<String>{
-    match cheak(&connection, user){
-        true => content::Json("{'status': 'True'}".to_string()),
-        false => content::Json("{status: 'False'}".to_string())
+    match cheak(&connection, user) {
+        Ok(state) => {match state{ 
+            true => content::Json("{'status': 'True'}".to_string()),
+            false => content::Json("{'status': 'False'}".to_string())}
+        },
+        Err(err) => {println!("{}", err); content::Json("{'status': 'Error'}".to_string())}
     }
 }
 
-fn cheak(connection: &LogsDbConn, user: Json<Member>) -> bool {
+
+
+fn cheak(connection: &LogsDbConn, user: Json<Member>) -> Result<bool, rusqlite::Error> {
     let name = &user.name;
     let password = hashing(&user.password) + "oBI$Xg(Z?3w]SyE_UW2n";
     let query = format!("SELECT * FROM users WHERE name = '{}' AND password = '{}'", name, password);
-    println!("{}", query);
-    let mut data = connection
-        .prepare(&query).unwrap();
-
+    let mut data = match connection.prepare(&query){
+        Ok(data) => data,
+        Err(err) => return Err(err)
+        };
     let mut is: bool = false;
     let person_iter = data.query_map(&[], |row| {
         User {
@@ -75,21 +77,20 @@ fn cheak(connection: &LogsDbConn, user: Json<Member>) -> bool {
         break
     }
     println!("{}", is);
-    is
+    Ok(is)
 }
+
+
 
 #[get("/")]
-fn get(connection: LogsDbConn) -> content::Json<String> {
-    content::Json(got(&connection))
+fn get(connection: LogsDbConn) -> Json<Vec<User>> {
+    got(&connection)
 }
 
-fn got (connection: &LogsDbConn) -> String {
+fn got (connection: &LogsDbConn) -> rocket_contrib::json::Json<Vec<User>> {
     let mut statement = connection
         .prepare("SELECT * FROM users")
         .unwrap();
-    let mut names: [String; 32] = Default::default();
-    let mut i = 0;
-
     let person_iter = statement.query_map(&[], |row| {
         User {
             name: row.get(0),
@@ -97,26 +98,13 @@ fn got (connection: &LogsDbConn) -> String {
             email: row.get(2),
         }
     }).unwrap();
+    let mut vec: Vec<User> = Vec::new();
     for person in person_iter {
         let a = person.unwrap();
-        names[i] = format!("name: {}, password {}, emeil {}", a.name, a.password, a.email);
-        println!("{}", names[i]);
-        i += 1
+        let user: User = User {name: a.name, password: a.password, email: a.email};
+        vec.push(user);
     }
-    let mut st: String = String::from("");
-    st += "{\n";
-    for j in 0..i {
-        st += "user";
-        st += &(j+1).to_string();
-        st += ": {";
-        st += &names[j];
-        match j == i-1 {
-        true => st += "}\n",
-        false => st += "},\n"
-        }
-    }
-    st +="}";
-    st 
+    Json(vec)
 }
 
 #[post("/", format = "application/json", data = "<user>")]
