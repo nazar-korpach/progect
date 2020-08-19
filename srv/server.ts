ab31ac125f1666c9f1fdee8b38296639ed6abec0
UAdeadUA-PC\UAdeadUA <korpach.nazar@gmail.com>
2020-08-19 19:35:40 +0300

    changed query

--- srv/server.ts
+++ srv/server.ts
@@ -22,9 +22,9 @@ app.post('/', async (request, response) => {
   const password: string = salt + createHash('sha256').update(request.body.password).digest('hex')
   const email: string = request.body.email
   const sql: string = 'INSERT INTO users '
-  const sq: object = { sql: sql, name: name, password: password, email: email }
+  const sq: object = { name: name, password: password, email: email }
   console.log(sql)
-  response.send(await query(sq))
+  response.send(await create('users', sq))
 })
 
 app.post('/login', async (request, response) => {
@@ -35,77 +35,108 @@ app.post('/login', async (request, response) => {
   const login: string = request.body.name
   console.log(request.body.password)
   const password: string = salt + createHash('sha256').update(request.body.password).digest('hex')
-  const sql: string = 'SELECT * FROM users WHERE '
   console.log(login, password)
-  const sq = { sql: sql, name: login, password: password }
-  const arr: Array<object> = await query(sq)
-  console.log(arr)
-  console.log(arr.length)
+  const sq = { name: login, password: password }
+  const arr: Array<object> = await find('users', sq)
+  console.log(arr, arr.length)
   if (arr.length === 0) response.send({ status: false })
   else response.send({ status: true })
 })
 
 app.delete('/', async (request, response) => {
-  const name: string = request.query.name.toString()
-  const sql: string = 'DELETE FROM users WHERE '
-  const sq: object = { sql: sql, name: name }
-  response.send(await query(sq))
+  const name: string = request.query.name ? request.query.name.toString() : ''
+  const sq: object = { name: name }
+  response.send(await destroy('users', sq))
 })
 
 app.get('/', async (request, response) => {
-  const sql: string = 'SELECT * FROM users'
-  console.log(sql)
   console.log(request.query)
-  const sq = { sql: sql }
-  response.send(await query(sq))
+  response.send(await find('users'))
 })
 
 app.listen(3000)
 
-const toSQL = (where: Record<string, any>):Record<string, any> => {
-  let sql: string = where.sql
+const find = (table: string, where: Record<string, any> = {}):Promise<Array<object>> => {
+  let sql: string = 'SELECT * FROM ' + table
   const param: Array<string> = []
-  delete where.sql
   const keys: Array<string> = (Object.keys(where))
   if (keys.length > 0) {
-    if (sql.slice(0, 6) === 'INSERT') {
-      console.log('insert')
-      sql += '('
-      for (const i in where) {
-        sql += i + ', '
-        param.push(where[i])
-      }
-      sql = sql.slice(0, sql.length - 2)
-      sql += ') VALUES ('
-      for (const i in where) {
-        sql += '?, '
-      }
-      sql = sql.slice(0, sql.length - 2) + ')'
-      console.log(sql, param)
-      return { sql: sql, params: param }
-    }
+    sql += ' WHERE '
     for (const i in where) {
       sql += (i + ' = ? AND ')
       param.push(where[i])
     }
     sql = sql.slice(0, sql.length - 4)
   }
-  return { sql: sql, params: param }
-}
-
-const query = (where: Record<string, any>):Promise<Array<object>> => {
-  return new Promise((resolve, reject) => {
-    const buffer: Record<string, any> = toSQL(where)
-    const sql = buffer.sql
-    const param = buffer.params
+  console.log(sql, param)
+  return new Promise<any>((resolve, reject) => {
     db.all(sql, param, (err, rows) => {
       if (err) {
-        throw err
+        reject(err)
+        return
       }
       rows.forEach((row) => {
         console.log(row)
       })
       resolve(rows)
     })
-  })
+  }).catch(err => console.log(err))
+}
+
+const create = (table: string, where: Record<string, any>):Promise<Array<object>> => {
+  let sql: string = 'INSERT INTO ' + table
+  const param: Array<string> = []
+  const keys: Array<string> = (Object.keys(where))
+  if (keys.length === 3) {
+    console.log('insert')
+    sql += '('
+    for (const i in where) {
+      sql += i + ', '
+      param.push(where[i])
+    }
+    sql = sql.slice(0, sql.length - 2)
+    sql += ') VALUES ('
+    for (const i in where) {
+      sql += '?, '
+    }
+    sql = sql.slice(0, sql.length - 2) + ')'
+    console.log(sql, param)
+  } else return
+  return new Promise<any>((resolve, reject) => {
+    db.all(sql, param, (err, rows) => {
+      if (err) {
+        reject(err)
+      }
+      rows.forEach((row) => {
+        console.log(row)
+      })
+      resolve(rows)
+    })
+  }).catch(err => console.log(err))
+}
+
+const destroy = (table: string, where: Record<string, any> = {}):Promise<Array<object>> => {
+  let sql: string = 'DELETE FROM ' + table
+  const param: Array<string> = []
+  const keys: Array<string> = (Object.keys(where))
+  if (keys.length > 0) {
+    sql += ' WHERE '
+    for (const i in where) {
+      sql += (i + ' = ? AND ')
+      param.push(where[i])
+    }
+    sql = sql.slice(0, sql.length - 4)
+  } else return
+  console.log(sql, param)
+  return new Promise<any>((resolve, reject) => {
+    db.all(sql, param, (err, rows) => {
+      if (err) {
+        reject(err)
+      }
+      rows.forEach((row) => {
+        console.log(row)
+      })
+      resolve(rows)
+    })
+  }).catch(err => console.log(err))
 }
